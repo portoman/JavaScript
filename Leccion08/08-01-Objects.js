e22468de0776b7008a401810e2d11419645ef145
portobuj <portbuj@yahoo.es>
2021-05-28 16:35:13 +0200

    Update 08-01-Objects.js

--- Leccion08/08-01-Objects.js
+++ Leccion08/08-01-Objects.js
@@ -1,22 +1,21 @@
 //Definicion de un objeto. En Java se instancian objetos a partir de clases, en JS se pueden crear objetos directamente
 
-let persona={
-    nombre:"Alfonso",
-    apellido:"Porto",
-    email:"portbuj@yahoo.es",
-    telefono:620771628,
-    idioma:"es",
-    get lang(){
-        return this.idioma.toUpperCase();
-    },
-    set lang( lang ){
-        this.idioma=lang.toUpperCase();
-    },
-    get nombreCompleto(){
-        return this.nombre+" "+this.apellido;
-    },
-   
-}
+let persona = {
+  nombre: "Alfonso",
+  apellido: "Porto",
+  email: "portbuj@yahoo.es",
+  telefono: 620771628,
+  idioma: "es",
+  get lang() {
+    return this.idioma.toUpperCase();
+  },
+  set lang(lang) {
+    this.idioma = lang.toUpperCase();
+  },
+  get nombreCompleto() {
+    return this.nombre + " " + this.apellido;
+  },
+};
 
 console.log(persona.nombre);
 console.log(persona.apellido);
@@ -24,17 +23,17 @@ console.log(persona.email);
 console.log(persona.telefono);
 console.log(persona.nombreCompleto);
 
-persona.lang="en";
+persona.lang = "en";
 console.log(persona.lang);
 
 console.log(persona);
 
 //Otra forma de definir objetos
 
-let persona2=new Object();
-persona2.nombre="Pepito";
-persona2.apellido="Garcia";
-persona2.telefono="58975489";
+let persona2 = new Object();
+persona2.nombre = "Pepito";
+persona2.apellido = "Garcia";
+persona2.telefono = "58975489";
 
 console.log(persona2);
 
@@ -45,89 +44,108 @@ console.log(persona["nombre"]);
 
 //For in: Para recorrer todo el objeto
 
-for(nombrePropiedad in persona){
-    console.log(nombrePropiedad);
-    console.log(persona[nombrePropiedad]);
+for (nombrePropiedad in persona) {
+  console.log(nombrePropiedad);
+  console.log(persona[nombrePropiedad]);
 }
 
 //Agregar y eliminar atributos
 
-persona.direccion="Coruña";
+persona.direccion = "Coruña";
 console.log(persona);
 
-persona.direccion="Coruña City";
+persona.direccion = "Coruña City";
 console.log(persona);
 
 delete persona.direccion;
 console.log(persona);
 
-
 //Distintas formas de imprimir objetos
 
-    console.log(persona);
+console.log(persona);
 
-    console.log(persona.nombre+" , "+persona.apellido+" ,"+persona.email);
+console.log(persona.nombre + " , " + persona.apellido + " ," + persona.email);
 
-    for(nombrePropiedad in persona){
-        console.log(persona[nombrePropiedad]);
-    }
+for (nombrePropiedad in persona) {
+  console.log(persona[nombrePropiedad]);
+}
 
-    let personaArray=Object.values(persona);
-    console.log(personaArray);
+let personaArray = Object.values(persona);
+console.log(personaArray);
 
-    let personaString=JSON.stringify(persona);
-    console.log(personaString);
+let personaString = JSON.stringify(persona);
+console.log(personaString);
 
-    //Constructor de objetos de tipo persona
+//Constructor de objetos de tipo persona
 
-    function Persona(nombre, apellido, email, telefono){
-        this.nombre=nombre;
-        this.apellido=apellido;
-        this.email=email;
+function Persona(nombre, apellido, email, telefono) {
+  this.nombre = nombre;
+  this.apellido = apellido;
+  this.email = email;
 
-        this.nombreCompleto = function(){
-            return this.nombre+ " "+this.apellido;
-        }
-    } 
+  this.nombreCompleto = function () {
+    return this.nombre + " " + this.apellido;
+  };
+}
 
-    let padre=new Persona("Paco", "Buyo", "pbuyo@hotmail.com", "123456");
+let padre = new Persona("Paco", "Buyo", "pbuyo@hotmail.com", "123456");
 
-    let madre=new Persona("Belen", "Perez", "bperez@hotmail.com", "987654");
+let madre = new Persona("Belen", "Perez", "bperez@hotmail.com", "987654");
 
-    madre.nombre="Isabel";
+madre.nombre = "Isabel";
 
-    console.log(padre);
-    console.log(madre);
+console.log(padre);
+console.log(madre);
 
-    madre.nombreCompleto;
+madre.nombreCompleto;
 
-    console.log(madre.nombreCompleto());
+console.log(madre.nombreCompleto());
 
-    console.log(padre);
-    console.log(madre);
+console.log(padre);
+console.log(madre);
 
-    Persona.prototype.telefono="444444444";
+Persona.prototype.telefono = "444444444";
 
-    console.log(padre.telefono);
-    console.log(madre.telefono);
+console.log(padre.telefono);
+console.log(madre.telefono);
 
-    console.log(padre);
-    console.log(madre);
-    
-    //Distintas formas de declarar objetos
+console.log(padre);
+console.log(madre);
 
-    let miObjeto=new Object();
-    let miObjeto2={};
+//Distintas formas de declarar objetos
 
-    let miCadena1=new String("Hola");
-    let miCadena2="Hola";
+let miObjeto = new Object();
+let miObjeto2 = {};
 
-    let miNumero=new Number(1);
-    let miNumero2=1;
+let miCadena1 = new String("Hola");
+let miCadena2 = "Hola";
 
-    let miBoolean=new Boolean(true);
-    let miBoolean2=true;
+let miNumero = new Number(1);
+let miNumero2 = 1;
 
-    let miArray=new Array();
-    let miArray2=[];
+let miBoolean = new Boolean(true);
+let miBoolean2 = true;
 
+let miArray = new Array();
+let miArray2 = [];
+
+//Call
+
+let persona3 = {
+  nombre: "Juan",
+  apellido: "Perez",
+  nombreCompeto: function () {
+    return this.nombre + " " + this.apellido;
+  },
+};
+
+let persona4={
+    nombre: "Carlos",
+    apellido: "Lara"
+}
+
+//Uso de call para usar el metodo persona1.nombreCompleto con los datos del persona2
+
+console.log(persona3.nombreCompeto());
+
+console.log(persona3.nombreCompeto.call(persona4));
