641c5b5c02cbdf231e731b8add83ae347680cd8e
portobuj <portbuj@yahoo.es>
2021-06-01 20:19:03 +0200

    Update 20210601

--- Leccion09/Persona.js
+++ Leccion09/Persona.js
@@ -1,7 +1,7 @@
 class Persona {
   static contadorPersonas = 0;
 
-  constructor(idPersona, nombre, apellido, edad) {
+  constructor(nombre, apellido, edad) {
     this._idPersona = ++Persona.contadorPersonas;
     this._nombre = nombre;
     this._apellido = apellido;
