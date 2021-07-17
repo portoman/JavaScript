3e60d07e2830e605108f3736eb0b9553c0ca20f6
portobuj <portbuj@yahoo.es>
2021-07-17 11:04:54 +0200

    Update 20210717

---
+++ MasterJS/angular/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+//Decorador
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+//Clase
+export class AppComponent {
+
+  //Atributo
+  public title = 'Angular con Alfonso Porto';
+}
---
+++ MasterJS/angular/src/app/videojuego/videojuego.component.ts
@@ -0,0 +1,21 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'videojuego',
+    template: `
+    <h2>Componente de Videojuegos</h2>
+    <ul>
+        <li>GTA</li>
+        <li>Prince of Persia</li>
+        <li>Tekken</li>
+        <li>Mario</li>
+    </ul>
+    `
+})
+
+export class VideojuegoComponent{
+
+    constructor(){
+        console.log("Se ha cargado el componente: videojuego.component.ts");
+    }
+}
