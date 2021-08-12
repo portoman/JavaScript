03d9aede6a0703cfab9533f08e2426fa416984fc
portobuj <portbuj@yahoo.es>
2021-08-12 12:19:27 +0200

    Update 20210812

--- MasterJS/08-Sockets/server/index.js
+++ MasterJS/08-Sockets/server/index.js
@@ -1,7 +1,11 @@
 var express = require('express');
 var app=express();
 var server=require('http').Server(app);
-var io=require('socket.io')(server);
+var io=require('socket.io')(server,{
+    cors:{
+        origin:'*'
+    }
+});
 
 app.use(express.static('client'));
 
@@ -9,9 +13,22 @@ app.get('/hola-mundo',function(req, res){
     res.status(200).send('Hola mundo desde una ruta');
 });
 
+var messages=[
+    {id:1,
+    text: 'Bienvenido al chat privado de Alfonso Porto',
+    nickname: 'Bot'}
+];
+
 io.on('connection',function(socket){
     console.log("El cliente con IP: "+socket.handshake.address+" se ha conectado...");
+    
+    socket.emit('messages', messages);
 
+    socket.on('add-message', function(data){
+        messages.push(data);
+
+        io.sockets.emit('messages',messages);
+    })
 });
 
 server.listen(6677, function(){
