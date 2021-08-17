7601d9616470abbe543fb0aff1026301b3291142
portobuj <portbuj@yahoo.es>
2021-08-17 11:31:20 +0200

    Update 20210817

--- MasterJS/04-POO-TypeScript/js/Repaso/programa.ts
+++ MasterJS/04-POO-TypeScript/js/Repaso/programa.ts
@@ -1,3 +1,33 @@
+//Módulo
+module Tienda{
+    export class Ropa{
+        constructor(public titulo: string){
+            alert(titulo);
+        }
+    }
+    export class Informatica{
+        constructor(public titulo: string){
+            alert('Tienda de tecnologia: '+titulo)
+        }
+    }
+}
+
+
+import Informatica=Tienda.Informatica;
+let cargar_informatica=new Informatica('supertienda');
+
+
+//Decorador
+function arranque(lanzar: string){
+    return function(target: Function){
+        target.prototype.lanzamiento= function():void{
+            alert(lanzar);
+        }
+    }
+} 
+
+
+@arranque("Lanzamiento del curso")
 class Programa{
     public nombre: string;
     public version: number;
@@ -24,4 +54,46 @@ class EditorVideo extends Programa{
     setTimeline(timeline: number){
         this.timeline=timeline;
     }
+
+    getTimeline(){
+        return this.timeline;
+    }
+
+    getAllData():string{
+        return this.getNombre()+" - "+this.getVersion()+" - "+this.getTimeline();
+    }
+}
+
+var programa=new Programa();
+
+programa.lanzamiento();
+
+var editor=new EditorVideo();
+editor.setNombre("Camtasia Studio");
+editor.setVersion(8);
+editor.setTimeline(4000);
+
+console.log(editor.getAllData());
+
+//Lógica del formulario
+
+var programas= [];
+
+function guardar(){
+    var nombre=(<HTMLInputElement>document.getElementById("nombre")).value.toString();
+
+    var programa=new Programa();
+    programa.setNombre(nombre);
+    programa.setVersion(1);
+
+    programas.push(programa);
+
+    var list="";
+    for(var i=0; i<programas.length; i++){
+        list=list+"<li>"+programas[i].getNombre()+"</li>";
+    }
+
+    var listado=<HTMLElement> document.getElementById("listado");
+
+    listado.innerHTML=list;
 }
